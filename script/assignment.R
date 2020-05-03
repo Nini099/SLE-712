08d4be3d0d00bf79ba900da5a9e460beff5647e6
Your Name <you@example.com>
2020-05-03 21:34:45 +1000

    Mean

--- script/assignment.R
+++ script/assignment.R
@@ -1,10 +1,22 @@
-
-x= gene_expression
-x
-x$SRR5150592
-x$SRR5150593
-x[1:6,1:2]
-x["Mean"] <- c((x$SRR5150592+x$SRR5150593)/2)
+#Question 1.Read in the file, making the gene accession numbers the row names. Show a table of values for the first six genes
+list.files()
+x<-read.table("data/gene_expression.tsv")
 head(x)
+str(x)
+x <- read.table("data/gene_expression.tsv", header = TRUE)
+head(x)
+str(x)
+x<- read.table("data/gene_expression.tsv", header = TRUE, stringsAsFactors = FALSE)
+head(x)
+str(x)
 
+x[1:6,1:2]
+#Question 2. Make a new column which is the mean of the other columns. Show a table of values for the first six genes.
+x$Mean_cal = rowMeans(x[,c(2,3)])
+x
+head(x)
+#
+sort()
 subset(x,Mean<10)
+
+
