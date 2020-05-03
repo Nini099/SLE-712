ba8d1873efc72ec230128f7a08f0183ef35a946d
Your Name <you@example.com>
2020-05-03 21:59:41 +1000

    deleted one row

--- script/assignment.R
+++ script/assignment.R
@@ -14,8 +14,9 @@ x[1:6,1:2]
 #Question 2. Make a new column which is the mean of the other columns. Show a table of values for the first six genes.
 x$Mean_cal = rowMeans(x[,c(2,3)])
 x
-head(x)
-#
+x$Mean<-NULL
+x
+
 sort()
 subset(x,Mean<10)
 
