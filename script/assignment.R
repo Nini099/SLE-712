a4b74a3442482c236f9546c73654832167e68f5d
Your Name <you@example.com>
2020-05-07 02:57:45 +1000

    Rowmean changed for only the start and end of the study, from the mean of all columns

--- script/assignment.R
+++ script/assignment.R
@@ -14,7 +14,7 @@ x[1:6,1:2]
 #Question 2. Make a new column which is the mean of the other columns. Show a table of values for the first six genes.
 x$Mean_cal = rowMeans(x[,c(2,3)])
 x
-sort()
+max.col(x$Mean_cal)
 subset(x,Mean<10)
 
 #Question6. Import this csv file into an R object. What are the column names? 
@@ -27,5 +27,5 @@ head(y)
 #Name of the columns are- "site", "TreeID","Circumf_2004_cm", "Circumf_2009_cm", "Circumf_2014_cm " and "Circumf_2019_cm"
 
 #Question7. Calculate the mean and standard deviation of tree circumference at the start and end of the study at both sites. 
-y$Mean = rowMeans(y[,c(3,4,5,6)])
+y$Mean = rowMeans(y[,c(3,6)])
 y
