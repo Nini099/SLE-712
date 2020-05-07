466e48eb65bc41c3e282e5e60bbf7f715074785e
Your Name <you@example.com>
2020-05-07 12:02:22 +1000

    read file changes, mean column created again

--- script/assignment.R
+++ script/assignment.R
@@ -6,14 +6,14 @@ str(x)
 x <- read.table("data/gene_expression.tsv", header = TRUE)
 head(x)
 str(x)
-x<- read.table("data/gene_expression.tsv", header = TRUE, stringsAsFactors = FALSE)
+x<- read.table("data/gene_expression.tsv", header = TRUE, stringsAsFactors = FALSE, row.names = 1)
 head(x)
 str(x)
 
 x[1:6,1:2]
 #Question 2. Make a new column which is the mean of the other columns. Show a table of values for the first six genes.
-x$Mean_cal = rowMeans(x[,c(2,3)])
-x
+x$Mean_cal <- rowMeans(x)
+head(x)
 #Question 3. 
 order(x$Mean_cal)
 order(-x$Mean_cal)
@@ -30,6 +30,7 @@ y <- read.csv("data/growth_data.csv", header = TRUE)
 head(y)
 y <- read.csv("data/growth_data.csv", header = TRUE, stringsAsFactors = FALSE)
 head(y)
+str(y)
 #Name of the columns are- "site", "TreeID","Circumf_2004_cm", "Circumf_2009_cm", "Circumf_2014_cm " and "Circumf_2019_cm"
 
 #Question7. Calculate the mean and standard deviation of tree circumference at the start and end of the study at both sites. 
