1e286a50827e3091177bf15e1622a5ed7c85b200
Your Name <you@example.com>
2020-05-17 22:07:48 +1000

    Part 2 files downloaded

--- script/assignment.R
+++ script/assignment.R
@@ -66,5 +66,30 @@ wilcox.test(SW$Growth,NE$Growth)
 #PART 2 
 #The sequence allocated for me is 
 #ATGTCTTCAACACGTATGCCAGCATTGTTTTTAGGTCACGGTAGTCCGATGAACGTGCTGGAAGATAATTTGTATACCCGCAGCTGGCAGAAGTTGGGGATGACATTGCCACGCCCGCAAGCGATTGTGGTGGTTTCGGCTCACTGGTTTACCCGTGGAACAGGAGTGACCGCGATGGAGACGCCGCCCACGATTCATGACTTTGGTGGCTTCCCGCAGGCGCTGTACGATACGCATTATCCTGCTCCGGGTTCGCCTGCGCTGGCACAGCGTCTGGTTGAGCTGTTAGCGCCGATCCCGGTGACGCTGGATAAAGAAGCCTGGGGCTTTGACCACGGCTCGTGGGGCGTGCTGATTAAGATGTATCCTGACGCTGATATCCCGATGGTGCAGTTGAGTATCGACAGTAGCAAACCTGCCGCCTGGCATTTCGAAATGGGGCGCAAACTGGCAGCGCTGCGAGATGAAGGAATAATGTTGGTCGCCAGCGGTAACGTGGTGCATAACCTGCGCACAGTGAAGTGGCACGGTGATAGTTCACCGTATCCGTGGGCGACGTCGTTTAATGAGTATGTGAAAGCGAATCTGACGTGGCAAGGGCCAGTGGAACAACATCCTCTGGTGAATTACCTCGACCATGAAGGTGGCACGTTATCGAACCCAACGCCAGAGCATTATTTGCCGTTGTTGTATGTGTTAGGTGCGTGGGATGGGCAGGAGCCAATTACCATTCCGGTTGAGGGTATAGAAATGGGCAGCCTGAGTATGCTGTCGGTGCAGATAGGCTAA
-library("seqinr")
 
+
+library("seqinr")
+library("R.utils")
+library("rBLAST")
+library("ape")
+library("ORFik")
+library("Biostrings")
+unlink(download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part2_files/sample.fa",
+              destfile = "ecoligene"))
+download.file("ftp://ftp.ensemblgenomes.org/pub/bacteria/release-42/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/cds/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz",destfile = "ecoligene.gz")
+R.utils::gunzip("ecoligene.gz", overwrite= TRUE)
+makeblastdb("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa",dbtype = "nucl","-parse_seqids")
+
+if ( ! file.exists("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa") ) {
+  download.file("ftp://ftp.ensemblgenomes.org/pub/bacteria/release-42/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/cds/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz",
+                destfile = "Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz")
+}
+R.utils::gunzip("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz", overwrite= TRUE)
+makeblastdb("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa",dbtype = "nucl","-parse_seqids")
+
+#For Query
+if ( ! file.exists("sample.fa") ) {
+  download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part2_files/sample.fa",
+                destfile = "sample.fa")
+
+}
