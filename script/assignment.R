f04693b0dd1f7f015697955730e865cc77e67816
Your Name <you@example.com>
2020-05-18 13:06:33 +1000

    length and Gc content

--- script/assignment.R
+++ script/assignment.R
@@ -79,17 +79,25 @@ unlink(download.file("https://raw.githubusercontent.com/markziemann/SLE712_files
 download.file("ftp://ftp.ensemblgenomes.org/pub/bacteria/release-42/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/cds/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz",destfile = "ecoligene.gz")
 R.utils::gunzip("ecoligene.gz", overwrite= TRUE)
 makeblastdb("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa",dbtype = "nucl","-parse_seqids")
-
+#Question 1. 
 if ( ! file.exists("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa") ) {
   download.file("ftp://ftp.ensemblgenomes.org/pub/bacteria/release-42/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/cds/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz",
                 destfile = "Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz")
 }
 R.utils::gunzip("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz", overwrite= TRUE)
 makeblastdb("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa",dbtype = "nucl","-parse_seqids")
-
+#there are 4140 sequences present in the E.coli set. 
+#Question 2
 #For Query
 if ( ! file.exists("sample.fa") ) {
   download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part2_files/sample.fa",
                 destfile = "sample.fa")
 
 }
+Ecoli_Sample <- read.fasta("sample.fa")
+allocated_Seq <- Ecoli_Sample$`53`
+allocated_Seq [1:789]
+length(allocated_Seq)
+table(allocated_Seq)
+(247+181)*100/(173+181+247+188)#to calculate the proportion of GC
+#The proportion of GC is 54.24588% 
