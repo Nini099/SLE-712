2abc89d4829dad4e3ba204b615f830e6ad4462ca
Your Name <you@example.com>
2020-06-05 15:32:24 +1000

    changes for assessment

--- script/assignment.R
+++ script/assignment.R
@@ -9,60 +9,60 @@ output: html_document
 
 #Question 1.Read in the file, making the gene accession numbers the row names. Show a table of values for the first six genes
 
-x<-read.table("data/gene_expression.tsv") # 
-head(x)
-str(x)
-x <- read.table("data/gene_expression.tsv", header = TRUE)
-head(x)
-str(x)
+#Read the file and save it as "x" and making the gene accession numbers as row names 
+download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part1_files/gene_expression.tsv", destfile = "gene_expression.tsv" )
 x<- read.table("data/gene_expression.tsv", header = TRUE, stringsAsFactors = FALSE, row.names = 1)
 head(x)
-str(x)
+str(x) #to see the interanl structure of "x"
+#to make the table of first six genes in the table, where inside the bracket [row,colum] 
 x[1:6,1:2]
+
 #Question 2. Make a new column which is the mean of the other columns. Show a table of values for the first six genes.
+#to make a new row with mean values from other rows
 x$Mean_cal <- rowMeans(x)
 head(x)
-x[1:6,1:3]
+x[1:6,1:3] # as the new row is column three
+
 #Question 3. List the 10 genes with the highest mean expression
 order(-x$Mean_cal)
 x[order(-x$Mean_cal),]
 Highest_mean <-x[order(-x$Mean_cal),]
 head(Highest_mean,10)
 #question4  Determine the number of genes with a mean <10  
-subset(x,Mean_cal<10)
+filteredmean <-subset(x,Mean_cal<10)
+nrow(filteredmean)
 #Question 5.  Make a histogram plot of the mean values in png format and paste it into your report
 range(x$Mean_cal)
 hist(x$Mean_cal,main="Histogram for Mean values",xlab = "mean value",border = "red",col="blue",breaks = 50,xlim=c(0,10000))
 #Question6. Import this csv file into an R object. What are the column names? 
-y<- read.csv("data/growth_data.csv")
-head(y)
-str(y) 
-y <- read.csv("data/growth_data.csv", header = TRUE)
-head(y)
+#to download the file from provided link
+download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part1_files/growth_data.csv", destfile = "growth_data.csv" )
+#saved the file as object "y"
 y <- read.csv("data/growth_data.csv", header = TRUE, stringsAsFactors = FALSE)
 head(y)
 str(y)
-subset(y,Site=="northeast")
+
+#Question7. Calculate the mean and standard deviation of tree circumference at the start and end of the study at both sites. 
+# Subset two "sites" as two different objects for calculation
 NE <- subset(y,Site=="northeast")
 SW <- subset(y,Site=="southwest")
 head(NE)
 head(SW)
-#Name of the columns are- "site", "TreeID","Circumf_2004_cm", "Circumf_2009_cm", "Circumf_2014_cm " and "Circumf_2019_cm"
-
-#Question7. Calculate the mean and standard deviation of tree circumference at the start and end of the study at both sites. 
-mean(NE$Circumf_2004_cm)# Mean calculation of tree circumference at the start and end of the study for North East Site
+# Mean calculation of tree circumference at the start and end of the study for North East Site
+mean(NE$Circumf_2004_cm)
 mean(NE$Circumf_2019_cm)
 sd(NE$Circumf_2004_cm)
 sd(NE$Circumf_2019_cm)
-
-mean(SW$Circumf_2004_cm)#Mean  and standard deviation calculation of tree circumfurence at the satrt and end of the site Southwest
+#Mean  and standard deviation calculation of tree circumfurence at the satrt and end of the site Southwest
+mean(SW$Circumf_2004_cm)
 mean(SW$Circumf_2019_cm)
 sd(SW$Circumf_2004_cm)
 sd(SW$Circumf_2019_cm)
 
 # Question 8.Make a box plot of tree circumference at the start and end of the study at both sites.
-
+#boxplot function used with headings
 boxplot(NE$Circumf_2004_cm,NE$Circumf_2019_cm,SW$Circumf_2004_cm,SW$Circumf_2019_cm,names=c("NE2004","NE2019","SW2004","SW2019"),ylab="Circumference(cm)",main="Growth at 2 plantation sites")
+
 #Question 9. Calculate the mean growth over the past 10 years at each site.
 NE$Growth <- (NE$Circumf_2019_cm-NE$Circumf_2009_cm ) #new column for growth over the past 10 years for NorthEast 
 head(NE)
@@ -86,31 +86,32 @@ library("ORFik")
 library("Biostrings")
 
 #Question 1. Download the whole set of E. coli gene DNA sequences and use gunzip to decompress. Use the makeblast() function to create a blast database. How many sequences are present in the E.coli  set? 
-if ( ! file.exists("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa") ) {
-  download.file("ftp://ftp.ensemblgenomes.org/pub/bacteria/release-42/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/cds/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz",
-                destfile = "Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz")
-}
+#to download the file from provided link 
+download.file("ftp://ftp.ensemblgenomes.org/pub/bacteria/release-42/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/cds/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz",
+              destfile = "Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz") 
+#To unzip the files
 R.utils::gunzip("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz", overwrite= TRUE)
 makeblastdb("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa",dbtype = "nucl","-parse_seqids")
 #there are 4140 sequences present in the E.coli set. 
 
-#Question 2
-#For Query
+#Question 2Download the sample fasta sequences and read them in as above. For your allocated sequence, determine the length (in bp) and the proportion of GC bases
+#To download sample file 
 if ( ! file.exists("sample.fa") ) {
   download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part2_files/sample.fa",
                 destfile = "sample.fa")
   
 }
 Ecoli_Sample <- read.fasta("sample.fa")
+#renaming the allocated sequence
 allocated_Seq <- Ecoli_Sample$`53`
-allocated_Seq [1:789]
+
 seqinr::getLength(allocated_Seq)
 table(allocated_Seq)
 #to calculate the proportion of GC
 seqinr::GC(allocated_Seq)
 #The proportion of GC is 54.24588% 
 
-#Question 3.
+#Question 3.You will be provided with R functions to create BLAST databases and perform blast searches. Use blast to identify what E. coli  gene your sequence matches best. Show a table of the top 3 hits including percent identity, E-value and bit scores.
 source("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part2_files/mutblast_functions.R")
 myblastn_tab
 res <- myblastn_tab(allocated_Seq, db="Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa")
@@ -123,11 +124,14 @@ hits
 db <- read.fasta("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa")
 str(str(db[1:6]))
 
-#Quistion 4 # create a copy of my allocated sequnce that has certain number of mismathces in it
+#Quistion 4 You will be provided with a function that enables you to make a set number of point mutations to your sequence of interest. Run the function and write an R code to check the number of mismatches between the original and mutated sequence
+# create a copy of my allocated sequnce that has certain number of mismathces in it
 seqinr::write.fasta(allocated_Seq,names="allocated_Seq",file.out = "allocated_Seq.fa")
 makeblastdb("allocated_Seq.fa",dbtype="nucl", "-parse_seqids")
+#save it as res 
 res <- myblastn_tab(allocated_Seq, db="allocated_Seq.fa")
 res
+#taste with a specific number of mismatches
 my_allocated_mutator<-mutator(allocated_Seq,20)
 res <- myblastn_tab(my_allocated_mutator, db="allocated_Seq.fa") #
 
@@ -136,7 +140,7 @@ res
 
 
 
-limitmutation <-function(res){myblastn_tab(c(0,1),1,replace= TRUE)}
+limitmutation <-function(res){myblastn_tab(c(0,1),1)}
 replicate(215,limitmutation(res))
 mean(replicate(215,limitmutation(res)))
 
